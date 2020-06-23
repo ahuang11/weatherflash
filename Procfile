fa573cf1b1774baa13a52f44c9811c39912311e3
ahuang11 <ahuang11@illinois.edu>
2020-06-22 20:34:49 -0500

    Optimize startup and improve phone exp

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: panel serve --port=$PORT --num-procs=1 --allow-websocket-origin=weatherflash.herokuapp.com --address=0.0.0.0 --use-xheaders weatherflash.py
+web: panel serve --port=$PORT --num-procs=2 --allow-websocket-origin=weatherflash.herokuapp.com --address=0.0.0.0 --use-xheaders weatherflash.py
--- weatherflash.py
+++ weatherflash.py
@@ -40,6 +40,40 @@ CSS = """
         margin-left: 8%;
         margin-right: 8%;
     }
+
+    /* Tooltip container */
+    .tooltip {
+      position: relative;
+      display: inline-block;
+    }
+
+    /* Tooltip text */
+    .tooltip .tooltiptext {
+      visibility: hidden;
+      background-color: #555;
+      color: #fff;
+      text-align: center;
+      padding: 3px 5px;
+      border-radius: 6px;
+      min-width: 125px;
+
+      /* Position the tooltip text */
+      position: absolute;
+      z-index: 1;
+      bottom: 125%;
+      left: 50%;
+      margin-left: -25px;
+
+      /* Fade in tooltip */
+      opacity: 0;
+      transition: opacity 0.2s;
+    }
+
+    /* Show the tooltip text when you mouse over the tooltip container */
+    .tooltip:hover .tooltiptext {
+      visibility: visible;
+      opacity: 1;
+    }
 """
 pn.config.raw_css.append(CSS)
 
@@ -72,6 +106,10 @@ class WeatherFlash():
         _, self.name, _, _, _, self.ts, network = self.df_meta.loc[
             self.df_meta['stid'] == station.upper()].values[0]
 
+        if station.upper() in ['KCMI', 'CMI']:
+            self.df = pd.read_pickle('CMI.pkl')
+            return
+
         df = pd.read_csv(URL_FMT.format(station=station, network=network),
                          index_col='day', parse_dates=True)
         df = df.drop(columns='station').rename_axis('time')
@@ -94,39 +132,40 @@ class WeatherFlash():
             df.loc[df[col] < 0, col] = np.nan
         self.df = df
 
-    @staticmethod
-    def remove_white_borders(plot, element):
-        p = plot.state
-        p.border_fill_color = 'whitesmoke'
-
-    def create_hist(self, df_mday, var):
+    def create_hist(self, df_sel, var):
         # keep histogram pairs consistent with the same xlim + ylim
         # since the pairs are likely to be min + max or somehow related
         # for more intuitive comparison between the pairs
-        col_ind = list(df_mday.columns).index(var)
-        if col_ind % 2 == 0:
-            var_ref = df_mday.columns[col_ind + 1]
-        else:
-            var_ref = df_mday.columns[col_ind - 1]
-        var_min = df_mday[[var, var_ref]].min().min()
-        var_max = df_mday[[var, var_ref]].max().max()
+        if var == 'Precip In':
+            print(df_sel)
 
-        var_bins = np.linspace(var_min, var_max, 20).tolist()
+        col_ind = list(df_sel.columns).index(var)
+        if col_ind % 2 == 0:
+            var_ref = df_sel.columns[col_ind + 1]
+        else:
+            var_ref = df_sel.columns[col_ind - 1]
+        var_min = df_sel[[var, var_ref]].min().min()
+        var_max = df_sel[[var, var_ref]].max().max()
+        if var_max < 1:
+            var_max = 1
+
+        bins = 20 if len(df_sel) > 20 else 8
+        var_bins = np.linspace(var_min, var_max, bins).tolist()
         var_diff = np.diff(var_bins).min()
         if var_max == var_min:
             var_max += 0.01
         xlim = var_min - var_diff, var_max + var_diff
 
         var_freq, var_edge = np.histogram(
-            df_mday[var].values, bins=var_bins)
+            df_sel[var].values, bins=var_bins)
         var_ref_freq, _ = np.histogram(
-            df_mday[var_ref].values, bins=var_bins)
+            df_sel[var_ref].values, bins=var_bins)
         ymax = max(var_freq.max(), var_ref_freq.max())
         ylim = (0, ymax + ymax / 5)
 
         # manual implementation of the sharey functionality
         if col_ind % 4 == 0:
-            ylabel = 'Number of Years'
+            ylabel = 'Number of Days'
         else:
             ylabel = ''
 
@@ -136,12 +175,12 @@ class WeatherFlash():
         var_units = var_split[-1]
 
         var_hist = hv.Histogram((var_edge, var_freq)).opts(
-            xlim=xlim, ylim=ylim, xlabel='', ylabel=ylabel, title=''
+            xlim=xlim, ylim=ylim, xlabel='', ylabel=ylabel
         ).redim.label(x=var, Frequency=f'{var_field} Count')
 
         plot = var_hist
         # highlight selected date
-        var_sel = df_mday.loc[self.datetime.strftime('%Y-%m-%d'), var]
+        var_sel = df_sel.loc[self.datetime.strftime('%Y-%m-%d'), var]
         if np.isnan(var_sel):
             label = var
         else:
@@ -152,45 +191,80 @@ class WeatherFlash():
             var_hist_hlgt = var_hist[var_slice, :].opts(fill_color=RED)
             label = f'{var_field}: {var_sel:{var_fmt}} {var_units}'
             plot *= var_hist_hlgt
-        var_text = hv.Text(var_min, ylim[-1] - ymax / 20, label,
-                           halign='left', valign='top', fontsize=14)
-        plot *= var_text
 
         if var_freq.max() == 0:
-            plot *= hv.Text(
-                (var_min + var_max) / 2, ylim[-1] / 2, 'Data N/A', fontsize=18)
+            plot *= hv.Text(xlim[1] / 2, ylim[-1] / 2, 'Data N/A', fontsize=18)
 
         try:
-            var_climo = df_mday.iloc[0][f'Climo {var}']
+            var_climo = df_sel.iloc[0][f'Climo {var}']
             var_vline = hv.VLine(var_climo)
             plot *= var_vline
         except KeyError:
             pass
 
-        return plot
+        return plot.opts(title=label)
 
-    def create_plots(self):
+    @staticmethod
+    def create_hover_text(label, color, tooltip):
+        return pn.pane.HTML(
+            f'''
+            <div class="tooltip" style="border:0.5px; border-style:solid;
+            border-radius: 5px; padding: 4px 10px;
+            border-color:{color}; color:{color}">{label}
+            <span class="tooltiptext">{tooltip}</span></div>
+            ''', margin=(0, 5, 0, 0)
+        )
+
+    def create_highlights(self, label, df_sel):
+        row_sel = df_sel.loc[self.datetime]
+
+    def create_content(self):
         mday = str(self.datetime)[5:10]
-        df_mday = self.df.loc[self.df.index.strftime('%m-%d') == mday]
-        plots = hv.Layout([
-            self.create_hist(df_mday, var)
-            for var in self.df.columns[:-1]
-            if not var.startswith('Climo')
-        ]).cols(4).relabel(
-            f'{self.name.title()} ({self.station_input.value}) '
-            f'Weather on {self.datetime:%B %d}s since {self.ts[:4]}'
-        ).opts(toolbar=None)
-        return plots
+        df_sels = [self.df.loc[self.df.index.strftime('%m-%d') == mday]]
+        for days in [365, 180, 90, 30, 14, 7]:
+            df_sels.append(self.df.loc[
+                (self.df.index >= self.datetime - pd.Timedelta(days=days)) &
+                (self.df.index <= self.datetime)
+            ])
+
+        labels = ['Past Years', 'Past 365 Days', 'Past 180 Days',
+                  'Past 90 Days', 'Past 30 Days', 'Past 14 Days',
+                  'Past 7 Days']
+        tab_items = []
+        for label, df_sel in zip(labels, df_sels):
+            self.create_highlights(label, df_sel)
+
+            if 'Year' not in label:
+                time_label = df_sel.index.min()
+                weather_label = (f'Weather from {time_label:%B %d, %Y} to '
+                                 f'{self.datetime:%B %d, %Y}')
+            else:
+                time_label = self.ts[:4]
+                weather_label = (f'Weather on {self.datetime:%B %d}s '
+                                 f'since {time_label}')
+            plots = hv.Layout([
+                self.create_hist(df_sel, var)
+                for var in self.df.columns[:-1]
+                if not var.startswith('Climo')
+            ]).cols(4).relabel(
+                f'{self.name.title()} ({self.station_input.value}) '
+                f'{weather_label}'
+            ).opts(toolbar=None)
+            tab_items.append((
+                label, pn.pane.HoloViews(
+                    plots, linked_axes=False, min_width=900)
+                )
+            )
+        self.tabs[:] = tab_items
 
     def update_station_input(self, event):
         self.progress.active = True
         try:
             self.progress.bar_color = 'warning'
-            self.read_data(self.station_input.value)
-            plots = self.create_plots()
-            self.plots.object = plots
+            self.read_data(event.new)
+            self.create_content()
             self.progress.bar_color = 'secondary'
-        except Exception:
+        except Exception as e:
             self.progress.bar_color = 'danger'
         self.progress.active = False
 
@@ -198,18 +272,17 @@ class WeatherFlash():
         self.progress.active = True
         try:
             self.progress.bar_color = 'warning'
-            self.datetime = event.new
-            plots = self.create_plots()
-            self.plots.object = plots
+            self.datetime = pd.to_datetime(event.new)
+            self.create_content()
             self.progress.bar_color = 'secondary'
-        except Exception:
+        except Exception as e:
             self.progress.bar_color = 'danger'
         self.progress.active = False
 
     def view(self):
         self.station_input = pn.widgets.AutocompleteInput(
             name='ASOS Station ID', options=self.stations, align='center',
-            value='CMI', width=275)
+            value='CMI', width=250)
         self.station_input.param.watch(self.update_station_input, 'value')
 
         self.read_data(self.station_input.value)
@@ -217,36 +290,44 @@ class WeatherFlash():
 
         self.date_input = pn.widgets.DatePicker(
             name='Date Selector', align='center',
-            value=self.datetime.date(), width=275)
+            value=self.datetime.date(), width=250)
         self.date_input.param.watch(self.update_date_input, 'value')
 
-        plots = self.create_plots()
-
-        self.plots = pn.pane.HoloViews(
-            plots, sizing_mode='stretch_both')
-
         self.progress = pn.widgets.Progress(
-            active=False, bar_color='secondary', width=275,
+            active=False, bar_color='secondary', width=250,
             margin=(-15, 10, 25, 10), align='center')
 
         title = pn.pane.Markdown(
             f'# <center>Weather<span style='
             f'"color:{RED}">Flash</span></center>',
-            width=275, margin=(5, 10, -25, 10))
+            width=250, margin=(5, 10, -25, 10))
 
         subtitle = pn.pane.Markdown(
             f'<center>*Selected date\'s listed and '
-            f'highlighted in red; climatology shown as dashed line.</center>'
-            f'<a href="https://mesonet.agron.iastate.edu/request/daily.phtml" target="_blank">ASOS Data</a> | '
-            f'<a href="https://github.com/ahuang11/weatherflash" target="_blank">Source Code</a> | '
-            f'<a href="https://github.com/ahuang11/" target="_blank">Author\'s GitHub</a>*',
-            width=275, margin=(-5, 10))
+            f'highlighted in red; climatology shown as dashed line.\n'
+            f'<a href="https://mesonet.agron.iastate.edu/request/daily.phtml"'
+            f' target="_blank">ASOS Data</a> | '
+            f'<a href="https://github.com/ahuang11/weatherflash" '
+            f'target="_blank">Source Code</a> | '
+            f'<a href="https://github.com/ahuang11/" '
+            f'target="_blank">My GitHub</a>*</center>',
+            width=250, margin=(-5, 10))
+
+        self.highlights = pn.Row(
+            sizing_mode='stretch_width', margin=(5, 10, 0, 10))
+
+        left_col = pn.Column(
+            title, self.progress,
+            self.station_input, self.date_input, self.highlights,
+            subtitle, sizing_mode='stretch_height')
+
+        self.tabs = pn.Tabs(sizing_mode='stretch_both',
+            margin=(10, 35), dynamic=True, tabs_location='below'
+        )
+        self.create_content()
 
         layout = pn.Row(
-            pn.Column(title, self.progress,
-                      self.station_input, self.date_input,
-                      subtitle, sizing_mode='stretch_height'),
-            pn.Column(self.plots, sizing_mode='stretch_both', margin=(10, 35)),
+            left_col, self.tabs,
             sizing_mode='stretch_both'
         )
         return layout
